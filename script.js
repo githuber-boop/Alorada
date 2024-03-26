d2db424667b3533e2dc423961afb3bc428721cc7
Ashish <ashishalex084@gmail.com>
2024-03-26 22:12:39 +0530

    CSS

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const checkbox = document.getElementById("checkbox");
-checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("dark");
-  });
-
 const links = document.querySelector('.links')
 const hamburger = document.querySelector('.hamburger')
 const themeToggle = document.querySelector('.theme_toggle')
+const hamburgerLine = document.querySelector('.line')
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+    hamburgerLine.classList.toggle('white');
+  });
+
 
 hamburger.addEventListener('click' , () => {
   const visibility = links.getAttribute('data-visible');
   const themeToogleVisibility = themeToggle.getAttribute('data-visible')
+
+  hamburgerLine.classList.toggle('black');
+
 
   if (visibility === "false") {
     links.setAttribute("data-visible" , true)
