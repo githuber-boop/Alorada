12bcd546274957c3ab0df861ed3adde1692de675
Ashish <ashishalex084@gmail.com>
2024-03-26 15:17:07 +0530

    Second

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
     document.body.classList.toggle("dark");
-  })
+  });
+
+const links = document.querySelector('.links')
+const hamburger = document.querySelector('.hamburger')
+const themeToggle = document.querySelector('.theme_toggle')
+
+hamburger.addEventListener('click' , () => {
+  const visibility = links.getAttribute('data-visible');
+  const themeToogleVisibility = themeToggle.getAttribute('data-visible')
+
+  if (visibility === "false") {
+    links.setAttribute("data-visible" , true)
+  } else if (visibility === "true") {
+    links.setAttribute("data-visible" , false)
+  }
+
+
+
+})
